135aa25ed0873cbb587bb0f15ebc164e7a53e5de
visheskumaryadav <vishes.loveindia@gmail.com>
2023-05-31 21:00:37 +0530

    pattern full pyramid

--- Programs/src/Patterns/BasicPatterns.java
+++ Programs/src/Patterns/BasicPatterns.java
@@ -1,4 +1,37 @@
 package Patterns;
 
 public class BasicPatterns {
+    /*
+    Full pyramid
+      1
+     232
+    34543
+   4567654
+  567898765
+     */
+    static void printFullPyramid(int row){
+        int col=0;
+        for(int i=1;i<=row;i++){
+
+            for(int space=1;space<=row-i;space++)
+            {
+                System.out.print(" ");
+            }
+            col=i;
+            for(int j=1;j<=i;j++) //left side triangle
+            {
+                System.out.print(col++ + "");
+            }
+            col--;
+            for(int k=1;k<i;k++) // right side triangle
+            {
+                System.out.print(--col +"");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+        BasicPatterns.printFullPyramid(5);
+    }
 }
